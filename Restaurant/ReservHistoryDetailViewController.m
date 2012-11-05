417b63e3902145c579bb7ae398499362912d0d36
Matrix Soft <matrixsoft@Matrixs-MacBook-Pro.local>
2012-11-05 19:24:44 +0200

    looks like send device token instead of uuid

--- Restaurant/ReservHistoryDetailViewController.m
+++ Restaurant/ReservHistoryDetailViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ReservHistoryDetailViewController.h"
+#import "RestaurantAppDelegate.h"
 
 @interface ReservHistoryDetailViewController ()
 @property (strong, nonatomic) NSMutableData *responseData;
@@ -46,16 +47,19 @@
     [self setAllTitlesOnThisPage];
 
     NSMutableString *statusRequesString = [NSMutableString stringWithFormat:@"%@%@%@%@", [[NSUserDefaults standardUserDefaults] valueForKey:@"dbLink"], @"/Customer_Scripts/getStatusesReservations.php?", [[NSUserDefaults standardUserDefaults] valueForKey:@"DBid"], @"&UUID="];
-    if (![[NSUserDefaults standardUserDefaults] objectForKey:@"uid"])
-        {
-            NSString *uid = [self createUUID];
-            [[NSUserDefaults standardUserDefaults] setValue:uid forKey:@"uid"];
-            //9E3C884C-6E57-4D16-884F-46132825F21E
-            [[NSUserDefaults standardUserDefaults] synchronize];
-            [statusRequesString appendString: uid];
-        }
-    else
-        [statusRequesString appendString:[[NSUserDefaults standardUserDefaults] objectForKey:@"uid"]];
+     NSString *deviceToken = [(RestaurantAppDelegate *)[[UIApplication sharedApplication] delegate] testToken1];
+//    
+//    if (![[NSUserDefaults standardUserDefaults] objectForKey:@"uid"])
+//        {
+//            NSString *uid = [self createUUID];
+//            [[NSUserDefaults standardUserDefaults] setValue:uid forKey:@"uid"];
+//            //9E3C884C-6E57-4D16-884F-46132825F21E
+//            [[NSUserDefaults standardUserDefaults] synchronize];
+//            [statusRequesString appendString: uid];
+//        }
+//    else
+       // [statusRequesString appendString:[[NSUserDefaults standardUserDefaults] objectForKey:@"uid"]];
+    [statusRequesString appendString:deviceToken];
     
     statusRequesString = [statusRequesString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding].copy;
     
